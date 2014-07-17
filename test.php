45c5529c1b1b1dc3284599418806ddfe112b4b85
abinpaulr9747 <abipaulr@gmail.com>
2014-07-17 14:34:33 +0530

    test

---
+++ test.php
@@ -0,0 +1,73 @@
+<?php
+
+return array(
+
+    /*
+    |--------------------------------------------------------------------------
+    | Sandbox Mode
+    |--------------------------------------------------------------------------
+    |
+    | When your application is in sandbox mode, all API calls will be made
+    | against PayPal's sandbox servers for testing purposes.
+    |
+    */
+
+    'sandbox' => $_ENV['PAYPAL_SANDBOX'],
+
+    /*
+	|--------------------------------------------------------------------------
+	| API Username
+	|--------------------------------------------------------------------------
+	|
+	| This is the API username for the account that owns the application.
+	|
+	*/
+
+    'api-username' => $_ENV['PAYPAL_API_USERNAME'],
+
+    /*
+	|--------------------------------------------------------------------------
+	| API Passwrd
+	|--------------------------------------------------------------------------
+	|
+	| This is the API password for the account that owns the application.
+	|
+	*/
+
+    'api-password' => $_ENV['PAYPAL_API_PASSWORD'],
+
+    /*
+	|--------------------------------------------------------------------------
+	| API Signature
+	|--------------------------------------------------------------------------
+	|
+	| This is the API signature for the account that owns the application.
+	|
+	*/
+
+    'api-signature' => $_ENV['PAYPAL_API_SIGNATURE'],
+
+    /*
+	|--------------------------------------------------------------------------
+	| Raw API Dump
+	|--------------------------------------------------------------------------
+	|
+	| Enable / Disable the raw API request/response dump at the bottom of
+    | pages.
+	|
+	*/
+
+    'raw-api-dump' => true,
+
+    /*
+	|--------------------------------------------------------------------------
+	| API Logging
+	|--------------------------------------------------------------------------
+	|
+	| Enable / Disable API logs.
+	|
+	*/
+
+    'api-log' => true,
+
+);
